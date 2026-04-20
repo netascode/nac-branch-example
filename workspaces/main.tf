b9086871c55e101ca565d647fe57507ca23416d6
snmitrov <snmitrov@cisco.com>
2026-04-20 16:06:49 +0200

    Update main.tf

--- workspaces/main.tf
+++ workspaces/main.tf
@@ -1,5 +1,5 @@
 module "model" {
-  source           = "github.com/netascode/terraform-meraki-nac-meraki/modules/model"
+  source           = "github.com/netascode/terraform-meraki-nac-meraki/modules/model?ref=v0.6.0"
   yaml_directories = ["../data"]
   write_model_file = "merged_configuration.nac.yaml"
 }
